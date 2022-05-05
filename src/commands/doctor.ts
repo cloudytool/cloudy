cb64869d0ca7b190797d7e12e904a41bf8903a94
Ivan Fokeev <vanya@vanya.cc>
2022-05-05 19:55:01 +0300

    fix doctor

--- src/commands/doctor.ts
+++ src/commands/doctor.ts
@@ -9,10 +9,20 @@ export default class Doctor extends Command {
   ]
 
   public async run(): Promise<void> {
+    const hasGit = which.sync('git', {nothrow: true})
+    const hasYarn = which.sync('yarn', {nothrow: true})
     const hasPulumi = which.sync('pulumi', {nothrow: true})
 
+    if (!hasGit) {
+      this.error('Please install git (https://git-scm.com/downloads)')
+    }
+
+    if (!hasYarn) {
+      this.error('Please install yarn (https://classic.yarnpkg.com/lang/en/docs/install)')
+    }
+
     if (!hasPulumi) {
-      this.error('Please install pulumi (https://www.pulumi.com/docs/get-started/install/)')
+      this.error('Please install pulumi (https://www.pulumi.com/docs/get-started/install)')
     }
 
     this.log('** Everything looks good! **')
